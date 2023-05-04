caffcd71a6ce4fba6942e0a3f6e73d07cb73c6ff
Jamie L Smyth <MrJamieLSmyth@gmail.com>
2023-05-04 17:28:31 +0000

    Add shell for rental routes

--- route/route.go
+++ route/route.go
@@ -1,8 +1,20 @@
 package route
 
 import (
+    "net/http"
     "github.com/gin-gonic/gin"
 )
 
 func Init(router *gin.Engine) {
+    router.GET("/rentals", GetRentals)
+	router.GET("/rentals/:id", GetRental)
+}
+
+
+func GetRentals(context *gin.Context) {
+    context.IndentedJSON(http.StatusOK, struct {}{})
+}
+
+func GetRental(context *gin.Context) {
+    context.IndentedJSON(http.StatusOK, struct {}{})
 }
