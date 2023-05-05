ed53fe74a80c99b426e83b2952f224b05d734872
Jamie L Smyth <MrJamieLSmyth@gmail.com>
2023-05-05 03:02:15 +0000

    Move host and port to env vars with default

--- app.go
+++ app.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"outdoorsy.com/backend/route"
 )
@@ -9,7 +12,12 @@ func main() {
 
 	router := gin.Default()
 	route.Init(router)
-
-	router.Run(":8080") // TODO: make this configurable
+	// get application host and port from env so they can be configured for each environment in necessary
+	host := os.Getenv("APP_HOST")// Defaults to all network interfaces
+	port := os.Getenv("APP_PORT")
+	if len(port) == 0 {
+		port = "8080" //Default to 8080
+	}
+	router.Run(fmt.Sprintf("%s:%s", host, port)) // TODO: make this configurable
 
 }
